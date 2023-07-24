e39acdf5de37bdccd588b8ade695c7e507d4d319
Faith <fmschroeder98@gmail.com>
2023-07-24 18:54:45 -0500

    working on the astronaut code

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// TODO: add code here
+window.addEventListener('load', () => {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then((response) => {
+        response.json().then((json) => {
+            json.sort(function(a, b) {
+                return a.hoursInSpace < b.hoursInSpace;
+            });
+
+            let htmlBlock = document.getElementById('container');
+
+            htmlBlock.innerHTML += `
+            <h4>Number of Astronauts Displayed: ${json.length}</h4>
+            `
+            for (let i = 0; i < json.length; i++) {
+                htmlBlock.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li id="status_${json[i].id}">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}"/>
+                </div>
+                `;
+                if (json[i].active === true) {
+                    document.getElementById(`status_${json[i].id}`).style.color = "green";
+                }
+            }
+            
+        });
+    });
+});
